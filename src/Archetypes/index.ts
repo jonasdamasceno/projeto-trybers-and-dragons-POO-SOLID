6e8f919ff687cdebe59f9c02e9400d4a5edbfb33
jdamasceno <jonasmichael2021@outlook.com>
2024-02-06 00:53:02 -0300

    requisito 6

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,15 @@
 import Archetype from './Archetype';
 
+import Mage from './Mage';
+import Necromancer from './Necromanger';
+import Ranger from './Ranger';
+import Warrior from './Warrior';
+
 export default Archetype;
+
+export {
+  Mage,
+  Necromancer,
+  Ranger,
+  Warrior,
+};
